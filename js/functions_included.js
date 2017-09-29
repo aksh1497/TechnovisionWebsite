5024477f8aa49b8a535413dd52eb685e5ce8962a
Ankush Patil <aspraz2658@gmail.com>
2017-09-29 15:17:56 +0530

    changed

--- js/functions_included.js
+++ js/functions_included.js
@@ -1,3 +1,51 @@
+        function hasClass(el, className) {
+            if (el.classList)
+            return el.classList.contains(className)
+            else
+            return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'))
+        }
+        
+        function addClass(el, className) {
+            alert("hello");
+            if (el.classList)
+            el.classList.add(className)
+            else if (!hasClass(el, className)) el.className += " " + className
+        }
+        
+        function removeClass(el, className) {
+            if (el.classList)
+            el.classList.remove(className)
+            else if (hasClass(el, className)) {
+            var reg = new RegExp('(\\s|^)' + className + '(\\s|$)')
+            el.className=el.className.replace(reg, ' ')
+            }
+        }
+
+  
+function setaccomodationRules() {
+    var tablock = document.getElementById('ta_rules');
+    tablock.style.display = "none";
+    var accomodation_block = document.getElementById('accomodation_rules');
+    accomodation_block.style.display = "block";
+    
+    var rel = documen.getElmentById('extra_rules_buttons_ta');
+    var el = document.getElementById('extra_rules_buttons_ac');
+
+    removeClass(rel,"traveling_allowance-active")
+    removeClass(rel,"hvr-bubble-bottom");
+    removeClass(rel,"hvr-bubble-bottom");
+    
+    
+    addClass(el,"traveling_allowance-active");
+    addClass(el,"hvr-bubble-bottom");
+    addClass(el,"hvr-bubble-bottom-active");
+
+
+
+
+
+}
+
 
         
         function openNavigation() {
@@ -54,7 +102,26 @@
             openerHider.style.display = "block";
         }
         function settaRules() {
+            var accomodation_block = document.getElementById('accomodation_rules');
+            accomodation_block.style.display = "none";
             var tablock = document.getElementById('ta_rules');
-            tablock.style.display = "none";
-            alert("in");
+            tablock.style.display = "block";
+            
+            var el = documen.getElmentById('extra_rules_buttons_ta');
+            var rel = document.getElementById('extra_rules_buttons_ac');
+
+            removeClass(rel,"traveling_allowance-active")
+            removeClass(rel,"hvr-bubble-bottom");
+            removeClass(rel,"hvr-bubble-bottom");
+            
+            
+            addClass(el,"traveling_allowance-active");
+            addClass(el,"hvr-bubble-bottom");
+            addClass(el,"hvr-bubble-bottom-active");
+
+
+
         }
+        
+
+
